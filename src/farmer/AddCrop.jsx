fe3f4aaba7e5146c19401779bea69d74dc1559c9
Naresh Bushi <bushinaresh68@gmail.com>
2026-02-01 15:17:05 +0530

    dfg

--- src/farmer/AddCrop.jsx
+++ src/farmer/AddCrop.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api/axiosInstance";
+import api from "../api/axios";
+
 
 const AddCrop = () => {
   const navigate = useNavigate();

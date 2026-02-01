acb9a057050bee57868bfe223c84caf8454f7893
Nareshboosi <nareshbushi68@gmail.com>
2026-02-01 11:40:19 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // 🔥 ATTACH TOKEN TO EVERY REQUEST
